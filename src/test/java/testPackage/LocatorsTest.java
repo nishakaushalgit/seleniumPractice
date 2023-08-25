2464f04b0955343726e7ca7bf8825acc518082a4
nishaGitCollab <nisha.kaushal@yahoo.com>
2023-08-25 21:46:30 +0530

    new1

--- src/test/java/testPackage/LocatorsTest.java
+++ src/test/java/testPackage/LocatorsTest.java
@@ -1,16 +1,17 @@
 package testPackage;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
 
-public class Locators {
-	
-	public static void main(String[] args) throws InterruptedException {
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LocatorsTest {
+	@Test
+	public void f() throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/angularpractice/");
@@ -19,22 +20,23 @@ public class Locators {
 		driver.findElement(By.name("email")).sendKeys("kaushalnisha01@gmail.com");
 		driver.findElement(By.id("exampleInputPassword1")).sendKeys("123456");
 		driver.findElement(By.cssSelector("input[type='checkbox']")).click();
-		WebElement staticDropdown= driver.findElement(By.id("exampleFormControlSelect1"));
-		Select dropdown=new Select(staticDropdown);
+		WebElement staticDropdown = driver.findElement(By.id("exampleFormControlSelect1"));
+		Select dropdown = new Select(staticDropdown);
 		dropdown.selectByVisibleText("Female");
 		System.out.println(dropdown.getFirstSelectedOption().getText());
 		driver.findElement(By.id("inlineRadio1")).click();
 		driver.findElement(By.name("bday")).sendKeys("20/01/1989");
 		driver.findElement(By.cssSelector("input[class='btn btn-success']")).click();
-		System.out.println(driver.findElement(By.cssSelector("div[class=\'alert alert-success alert-dismissible\']")).getText());
-		//Assert.assertEquals(driver.findElement(By.cssSelector("div[class=\'alert alert-success alert-dismissible\']")).getText(), "Success! The Form has been submitted successfully!.");
-	
-		
+		System.out.println(
+				driver.findElement(By.cssSelector("div[class=\'alert alert-success alert-dismissible\']")).getText());
+		// Assert.assertEquals(driver.findElement(By.cssSelector("div[class=\'alert
+		// alert-success alert-dismissible\']")).getText(), "Success! The Form has been
+		// submitted successfully!.");
+
 //		System.out.println(driver.findElement(By.name("checkBoxOption1")).isSelected());
 //		driver.findElement(By.name("checkBoxOption1")).click();
 //		System.out.println(driver.findElement(By.name("checkBoxOption1")).isSelected());
 //		System.out.println(driver.findElement(By.cssSelector("input[type='checkbox']")).getSize());
 		driver.quit();
 	}
-
 }
